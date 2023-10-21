7678290a2c3e7546e4783273bd7c8df434ad8073
richards0904 <113106414+richards0904@users.noreply.github.com>
2023-10-21 18:35:35 +0700

    Penambahan Detail Info

--- lib/widgets/detail_screen.dart
+++ lib/widgets/detail_screen.dart
@@ -22,8 +22,91 @@ class DetailScreen extends StatelessWidget {
                     height: 300,
                     fit: BoxFit.cover,
                   ),
-                ))
+                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple.withOpacity(0.5),
+                    shape: BoxShape.circle),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_back,
+                  ),
+                ),
+              ),
+            )
           ],
+        ),
+        // Detail Info
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 16,
+              ),
+              // Info Atas nama candi dan tombol favorite
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    candi.name,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.favorite_border))
+                ],
+              ),
+              // Info Tengah (lokasi, dibangun, tipe)
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.place,
+                    color: Colors.red,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const SizedBox(
+                    width: 70,
+                    child: Text(
+                      "Lokasi",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.location}')
+                ],
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.calendar_month,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Dibangun',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(": ${candi.built}")
+                ],
+              ),
+              Row(
+                children: [],
+              ),
+            ],
+          ),
         )
       ]),
     );
